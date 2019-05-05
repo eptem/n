f58998d95488328c4acc036305cf3f0253077fa0
Eptem <mropp1@mail.ru>
2019-05-05 16:47:26 +0500

    Initial Commit

---
+++ citizen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  citizen
+//
+//  Created by Артем Жорницкий on 05/05/2019.
+//  Copyright © 2019 Артем Жорницкий. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
